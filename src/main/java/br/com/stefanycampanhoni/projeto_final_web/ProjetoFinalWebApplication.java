d033f22b041ecbe054a8e9f5eaefbe3b601f6daf
Stefany <scampanhoni@gmail.com>
2024-11-25 17:46:21 -0300

    inital commit

---
+++ src/main/java/br/com/stefanycampanhoni/projeto_final_web/ProjetoFinalWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.stefanycampanhoni.projeto_final_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinalWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFinalWebApplication.class, args);
+	}
+
+}
